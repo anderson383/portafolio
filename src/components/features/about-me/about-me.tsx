3071178f9df78c6ea3cc666ef56552ea6eb733ff
Anderson Vargas Sepulveda <andersonvargas383@gmail.com>
2023-03-22 12:12:00 -0500

    add about-me

---
+++ src/components/features/about-me/about-me.tsx
@@ -0,0 +1,121 @@
+import {
+  useEffect, useState
+} from 'react';
+import axiosIntance from '@/services/config/axios/AxiosConfig';
+import { Interweave } from 'interweave';
+import styles from './about-me.module.scss';
+import { useLottie } from 'lottie-react';
+
+interface AboutMeProps {
+  content: {
+    style: string;
+    name: string;
+    title:string;
+    link: string;
+    subtitle: string;
+    image: {
+      url: string
+    }
+  }
+  locale: string
+  page: string
+}
+
+const AboutMe:React.FC<AboutMeProps> = props => {
+  const {
+    title, subtitle, image
+  } = props.content;
+
+  const [jsonAnimation, setJsonAnimation] = useState({
+    urlIconGit: '',
+    urlMainImage: ''
+  });
+
+  useEffect(() => {
+    axiosIntance.get(image.url, {responseType: 'json'}).then(resp => {
+      setJsonAnimation(last => ({
+        ...last,
+        urlMainImage: resp.data
+      }));
+    });
+
+    axiosIntance.get('https://lottie.host/6a52efa3-38ea-4dcd-8e48-968f1042aa6b/bRaMeUp92U.json', {responseType: 'json'}).then(resp => {
+      setJsonAnimation(last => ({
+        ...last,
+        urlIconGit: resp.data
+      }));
+    });
+  }, []);
+
+  const options = {
+    animationData: jsonAnimation.urlMainImage,
+    height: 430,
+    loop: true
+  };
+
+  const option2 = {
+    animationData: jsonAnimation.urlIconGit,
+    height: 430,
+    loop: true
+  };
+
+  const { View: ViewAboutMe } = useLottie(options, {
+    height: 466,
+    width: 466
+  });
+
+  const { View: ViewGitIcon } = useLottie(option2, {
+    height: 66,
+    width: 66
+  });
+
+  return (
+    <div className={styles.about_me + ' '}>
+
+      <div className="container">
+
+        <div className={styles.title}>
+          <Interweave
+            tagName='div'
+            content={title}
+            attributes={{ className: styles.title }}
+          />
+        </div>
+
+        <div className={styles.content}>
+          <div className="">
+            <img className={styles.icon_bg_1}src="/img/ellipse-about-me-1.png" alt="" />
+            <img className={styles.icon_bg_2}src="/img/ellipse-about-me-2.png" alt="" />
+          </div>
+          <div className={styles.text}>
+            <p>{subtitle}</p>
+          </div>
+          <div className={styles.image}>
+            {ViewAboutMe}
+          </div>
+        </div>
+
+        <div className={styles.footer}>
+          {
+            ViewGitIcon
+          }
+        </div>
+      </div>
+    </div>
+  );
+};
+
+AboutMe.defaultProps = {
+  content: {
+    image: {url: ''},
+    link: '',
+    name: '',
+    style: '',
+    subtitle: '',
+    title: ''
+  },
+  locale: '',
+  page: ''
+};
+
+export default AboutMe;
--- src/constants/app-components.ts
+++ src/constants/app-components.ts
@@ -1,13 +1,14 @@
+import AboutMe from '@/components/features/about-me/about-me';
 import Banner from '@/components/features/banner/';
 
 const APP_COMPONENTS = {
+  'about-me': {
+    component: AboutMe,
+    theme: ''
+  },
   'main-banner': {
     component: Banner,
     theme: 'default'
-  },
-  '': {
-    component: Banner,
-    theme: ''
   }
 };
 
